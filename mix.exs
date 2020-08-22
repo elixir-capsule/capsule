fd7eb8ca6b1a6f2705b27d3142ba6f5558d8640a
T Floyd Wright <tfwright@gmail.com>
2020-08-22 17:40:16 -0500

    Bump version

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Capsule.MixProject do
     [
       app: :capsule,
       description: "Uploaded file management for Elixir",
-      version: "0.4.0",
+      version: "0.5.0",
       elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
