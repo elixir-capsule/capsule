74a2239ff90cb653283ae71ae74428c21afd8d00
T Floyd Wright <tfwright@gmail.com>
2020-08-22 17:39:48 -0500

    Timeout URI uploads

--- lib/upload.ex
+++ lib/upload.ex
@@ -8,9 +8,14 @@ end
 
 defimpl Capsule.Upload, for: URI do
   def contents(name) do
-    case :httpc.request(:get, {name |> URI.to_string() |> String.to_charlist(), []}, [],
-           body_format: :binary
-         ) do
+    download =
+      Task.async(fn ->
+        :httpc.request(:get, {name |> URI.to_string() |> String.to_charlist(), []}, [],
+          body_format: :binary
+        )
+      end)
+
+    case download |> Task.await(15_000) do
       {:ok, {{'HTTP/1.1', 200, 'OK'}, _headers, body}} -> {:ok, body}
       {:error, {reason}} -> {:error, reason}
     end
